2d59e39943a5c84d709458c6a2f3b6270af3fd8f
ATechnoHazard <amolele@gmail.com>
2020-08-28 16:18:43 +0530

    :bug: fix overflowing menu items by making the menu responsive
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -4,7 +4,7 @@ import "./loading.css";
 const Loading = () => {
   return (
     <div className="container">
-      <div classNameName="spinner">
+      <div className="spinner">
         <div className="double-bounce1"></div>
         <div className="double-bounce2"></div>
       </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import hamburger from "../../assets/images/hamburger.svg";
 
 const Navbar = (props) => {
   const items = ["portfolio", "contact"];
+  const onClickHamburger = (_) => {
+    const nav = document.getElementById("nav-header");
+    if (nav.className === "nav-container") {
+      nav.className += " opened";
+    } else {
+      nav.className = "nav-container";
+    }
+  };
 
   return (
     <div>
-      <header>
-        <Link className="nav-logo" to="/">
-          Spherical<span className="nav-logo-span">Kat</span>
-        </Link>
+      <header className="nav-container" id="nav-header">
+        <div id="nav-button" onClick={onClickHamburger}>
+          <img src={hamburger} alt="hamburger" />
+        </div>
         <nav>
           <ul className="nav-links">
+            <Link className="nav-logo" to="/">
+              Spherical<span className="nav-logo-span">Kat</span>
+            </Link>
             {items.map((item) => (
               <li key={item}>
                 <Link
